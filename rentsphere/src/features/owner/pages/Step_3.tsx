2f9fdc366f1411488ab1ef1436f93bb44c5e0765
rawita <rawita@gmail.com>
2026-01-29 01:31:15 +0700

    Merge main into step3-owner

---
+++ rentsphere/src/features/owner/pages/Step_3.tsx
@@ -0,0 +1,24 @@
+const Step_3 = () => {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "linear-gradient(180deg, #CEC9FF 0%, #7C7999 100%)",
+        padding: "40px",
+      }}
+    >
+      <div style={{ width: "100%" }}>
+        <h1
+          style={{
+          color: "#000000",
+          marginBottom: "24px",
+         }}
+        >
+          ตั้งค่าคอนโดมิเนียม
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Step_3;
